feabc69e310c693a75b48d47f1c9ed4346cfe722
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-09-13 22:37:18 +0530

    Update store.js

--- client/src/store.js
+++ client/src/store.js
@@ -2,12 +2,22 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 
 import allPlaces from "./redux/placeSlice";
 
+import TitleReducer from "./redux/findPlacebyTitle";
+
+import selectedTourist from "./redux/TouristDetails";
+
 const rootReducer = combineReducers({
-    AllData: allPlaces
+    AllData: allPlaces,
+    byTitle: TitleReducer,
+    TouristDetails: selectedTourist
 })
 
 const store = configureStore({
-    reducer: rootReducer
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        immutableCheck: false,
+        serializableCheck: false,
+      })
 })
 
 export default store
