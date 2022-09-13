6b61fa39b852a05469e9a27073e6ab6105a645ec
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-09-13 22:36:40 +0530

    Update landingpage.js

--- client/src/components/landingpage.js
+++ client/src/components/landingpage.js
@@ -1,34 +1,50 @@
 // import Alt from "./userpic"
+import React, { useEffect} from 'react';
+
+import Search from './formcomponents/search';
+
+import CreateDestinations from './formcomponents/createDestination';
+
+import LandingHead from './formcomponents/landingHead';
+
+import { fetchPlaces } from "../redux/placeSlice"
+
+import {useSelector, useDispatch} from 'react-redux';
 
 function Landing () {
+    const dispatch = useDispatch()
+    const {menu, loading} = useSelector((state) => state.AllData);
+
+    useEffect(() => {
+        dispatch(fetchPlaces());
+      },[]);
     return (
         <>
-            <header className="flex flex-row justify-between w-full p-4 shadow-lg shadow-gray-300">
-                <h1 className="text-2xl font-bold uppercase text-gray-600">expl<span className="text-sky-500">ore</span></h1>
-                <div className="flex flex-row space-x-4">
-                    <img src={``} alt="" className="rounded-full p-5 bg-violet-700"/>
-                    <button className="uppercase text-xl font-medium bg-blue-600 rounded-lg text-white px-3 py-1">sign in</button>
-                </div>
-            </header>
-
+            <LandingHead/>
+        
             <div className="grid grid-cols-[1100px_minmax(10px,_1fr)_0px] w-full">
-                <div className="bg-red-100 grid grid-cols-4 space-x-4">
+            {loading && <div className="bg-img-loader p-56 mt-5 bg-no-repeat"></div>}
+                <div className="grid grid-cols-4 space-x-3 p-5">
+                    {menu.map((item) => { 
+                        const base64String = btoa(String.fromCharCode(...new Uint8Array(item.image.data.data)));
+                        return <div className="grid grid-cols-1 rounded-3xl bg-slate-100 justify-items-start
+                         shadow-lg shadow-black h-2/5 my-5 box-border transition duration-700 hover:scale-105" key={item._id}>
+                            <div className="h-44 flex flex-row">
+                                <img src={`data:image/png;base64,${base64String}`} alt={`img-loader`} className="rounded-t-3xl object-cover"/>
+                            </div>
+                            <div className="flex flex-col px-4 h-44 space-y-4">
+                                <h2 className="text-slate-500">{item.tags}</h2>
+                                <h1 className="font-bold text-xl capitalize">{item.title}</h1>
+                                <p className="text-slate-500">{item.message}</p>
+                            </div>
+                        </div>
+                        
+                    })}
                 </div>
-                <div className="py-10 items-center justify-center justify-between space-y-6 px-2">
-                    <form className="flex flex-col space-y-5 shadow-lg shadow-slate-600 p-6 h-64 rounded-xl">
-                        <input type="text" placeholder="search *" className="py-2 px-4 border border-slate-400 rounded-lg"></input>
-                        <input type="text" placeholder="search tags *" className="py-2 px-4 border border-slate-400 rounded-lg"></input>
-                        <button className="bg-blue-600 text-white py-3 px-4 rounded-xl">search</button>
-                    </form>
-                    <form className="flex flex-col space-y-5 shadow-lg shadow-slate-600 p-6 h-[35em] rounded-xl">
-                        <h1 className="text-xl font-bold capitalize text-center">create a explore</h1>
-                        <input type="text" placeholder="Title *" className="py-2 px-4 border border-slate-400 rounded-lg"></input>
-                        <textarea type="text" placeholder="Message" className="py-2 px-4 border border-slate-400 rounded-lg h-28"></textarea>
-                        <input type="text" placeholder="Tags *" className="py-2 px-4 border border-slate-400 rounded-lg"></input>
-                        <input type="file" className="w-3/4"></input>
-                        <button className="bg-blue-700 text-white py-3 rounded-xl">submit</button>
-                        <button className="bg-red-600 text-white py-2 rounded-xl">clear</button>
-                    </form>
+
+                <div className="py-10 items-center justify-center justify-between space-y-6">
+                    <Search/>
+                    <CreateDestinations/>
                 </div>
             </div>
         </>
