988ea4b0e6125295d9e64e527c75c1692e1f2152
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-09-14 18:24:26 +0530

    Update formheader.js

--- client/src/components/formheader.js
+++ client/src/components/formheader.js
@@ -9,7 +9,7 @@ function FormHeader () {
         <>
             <header className="flex flex-row justify-between w-full p-4 shadow-lg shadow-gray-300">
                 <h1 className="text-2xl font-bold uppercase text-gray-600">expl<span className="text-sky-500">ore</span></h1>
-                <button className="uppercase text-xl font-medium bg-blue-600 rounded-lg text-white px-3 py-1" onChange={nextpage}>sign in</button>
+                <button className="uppercase text-xl font-medium bg-blue-600 rounded-lg text-white px-3 py-1" onClick={nextpage}>login</button>
             </header>
         </>
     )
