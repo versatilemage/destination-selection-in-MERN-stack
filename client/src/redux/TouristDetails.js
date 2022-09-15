5cb0cbe03c1d30fa71aabd7b86126b85ff9e7d35
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-09-15 23:20:47 +0530

    Update TouristDetails.js

--- client/src/redux/TouristDetails.js
+++ client/src/redux/TouristDetails.js
@@ -1,17 +1,19 @@
 import {createSlice} from '@reduxjs/toolkit';
 
-import axios from "axios";
+// import axios from "axios";
 
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
+// axios.defaults.withCredentials = true
+
 export const fetchTouristdetails = createAsyncThunk("cred/usercred", async() => {
     try{
-        const res = await fetch (`http://localhost:6001/findtourist`).then((data) => {
+        const res = await fetch(`http://localhost:6001/findtourist`,{withCredentials: true}).then((data) => {
             return data.json()
         })
         return res
     }catch(err) {
-        console.log(err)
+        return(err)
     }
 })
 
@@ -26,7 +28,7 @@ const getTouristCredentials = createSlice({
             state.loading = true
         },
         [fetchTouristdetails.fulfilled]: (state, action) => {
-            state.menu = action.payload.data;
+            state.menu = action.payload.user;
             state.loading = false
         },
         [fetchTouristdetails.rejected]: (state, action) => {
