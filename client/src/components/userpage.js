3b6f73892dcb09096b418a4d1705ea9ac00773a8
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-09-17 23:52:02 +0530

    Update userpage.js

--- client/src/components/userpage.js
+++ client/src/components/userpage.js
@@ -1,31 +1,55 @@
-import React, { useEffect, useState} from 'react';
+import React, { useEffect } from 'react';
 
-import axios from 'axios';
+import LandingHead from './formcomponents/landingHead';
 
-axios.defaults.withCredentials = true;
+import {useSelector, useDispatch} from 'react-redux';
+
+import { fetchClickedDestination } from '../redux/click';
+
+import { useNavigate } from 'react-router-dom';
+
+import Moment from "moment";
 
 function Userpage () {
-    const [user, setUser] = useState();
 
-    const sendingRequest = async() => {
-        const res = await axios.get("http://localhost:6001/findtourist", {
-            withCredentials: true
-        })
-        .catch(err => console.log(err))
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const {loading, destination} = useSelector((state) => state.clickDestination)
 
-        const data = await res.data;
-        return data;
-    }
+    // useEffect(() => {
+    //     dispatch(fetchClickedDestination())
+    // },[])
 
-    useEffect(() => {
-        sendingRequest().then((data) => setUser(data.user))
-      },[]);
+    console.log(destination)
+
+    // const forConsole = destination.map((item) => {
+    //     return 
+    // })
 
     return (
         <>
             <div>
-            {user && <h1 className="bg-red-200 p-6">{user.firstname}</h1>}
-            <h1>userpage</h1>
+                <LandingHead/>
+                {destination && destination.map((item) => {
+                    const myDate = Moment(item.time,"YYYYMMDD HH:mm").fromNow()
+                    const base64String = btoa(String.fromCharCode(...new Uint8Array(item.image.data.data)));
+                    return<div key={item._id} className="grid grid-cols-2 space-x-4 p-5 bg-slate-100 shadow-lg shadow-slate-600">
+                        <div className="space-y-5 grid grid-row-4 items-justify">
+                            <h1 className="text-2xl font-bold capitalize">{item.title}</h1>
+                            <h2 className="text-slate-500 text-xl">{item.tags}</h2>
+                            <p className="text-slate-700 text-xl">{item.message}</p>
+                            <div className="flex flex-col space-y-2">
+                                <h1 className="text-xl font-bold capitalize">created by: </h1>
+                                <h1 className="text-slate-500 text-lg">{myDate}</h1>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-row justify-end object-cover">
+                            <img src={`data:image/png;base64,${base64String}`} alt={`img-loader`} 
+                            className="rounded-3xl object-cover aspect-square w-[40em] h-[30em]"/>
+                        </div>
+                    </div>
+                })}
             </div>
         </>
     )
