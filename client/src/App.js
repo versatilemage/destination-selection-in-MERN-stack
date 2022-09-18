db2d0da4238dce274533db6b236a0c4e1ccb8dbc
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-09-18 22:13:16 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import {Route, Routes} from "react-router-dom";
 
+import {useSelector} from 'react-redux';
+
 import Login from "./components/login";
 
 import Signup from "./components/signup"
@@ -11,13 +13,14 @@ import Userpage from "./components/userpage";
 import Trail from "./components/trial";
 
 function App() {
+  const isLoggedin = useSelector((state) => state.onlineStatus)
   return (
     <>
       <Routes>
         <Route exact path="/" element={<Login/>}></Route>
         <Route path="/signup" element={<Signup/>}></Route>
         <Route path="/home" element={<Landing/>}></Route>
-        <Route path="/user" element={<Userpage/>}></Route>
+        {isLoggedin && <Route path="/user" element={<Userpage/>}></Route>}
         <Route path="/trial" element={<Trail/>}></Route>
       </Routes>
     </>
