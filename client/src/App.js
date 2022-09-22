f5559e44701c59beb9f2a5e9cd369688eb8b7bbf
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-09-22 22:46:27 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,14 +13,14 @@ import Userpage from "./components/userpage";
 import Trail from "./components/trial";
 
 function App() {
-  const isLoggedin = useSelector((state) => state.onlineStatus)
+  const {isLoggedIn} = useSelector((state) => state.onlineStatus)
   return (
     <>
       <Routes>
         <Route exact path="/" element={<Login/>}></Route>
         <Route path="/signup" element={<Signup/>}></Route>
-        <Route path="/home" element={<Landing/>}></Route>
-        {isLoggedin && <Route path="/user" element={<Userpage/>}></Route>}
+        {isLoggedIn && <Route path="/home" element={<Landing/>}></Route>}
+        {isLoggedIn && <Route path="/user" element={<Userpage/>}></Route>}
         <Route path="/trial" element={<Trail/>}></Route>
       </Routes>
     </>
